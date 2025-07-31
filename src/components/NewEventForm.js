385ee42da32e1162bc12f0c2b934f29c383d606b
Majd Aldein Alhourani <majd.aldein.alhourani@gmail.com>
2025-07-31 13:34:13 +0300

    mplement controlled form inputs using onChange and value props

--- src/components/NewEventForm.js
+++ src/components/NewEventForm.js
@@ -1,17 +1,42 @@
+import { useState } from "react";
 import "./NewEventForm.css";
 
 export default function NewEventForm() {
+  const [title, setTitle] = useState("");
+  const [date, setDate] = useState("");
+
+  const resetForm = () => {
+    setTitle("");
+    setDate("");
+  };
+
+  console.log(title);
+  console.log(date);
   return (
     <form className="new-event-form">
       <label>
         <span>Event Title:</span>
-        <input type="title" />
+        <input
+          type="title"
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+        />
       </label>
 
       <label>
         <span>Event Date:</span>
-        <input type="date" />
+        <input
+          type="date"
+          onChange={(e) => setDate(e.target.value)}
+          value={date}
+        />
       </label>
+
+      <p onClick={resetForm}>Reset Form</p>
+
+      <p>
+        Title:{title}, Date: {date}
+      </p>
     </form>
   );
 }
