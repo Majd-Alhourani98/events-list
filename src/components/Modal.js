bd90e271f353446b5c899d55996e5e1418810099
Majd Aldein Alhourani <majd.aldein.alhourani@gmail.com>
2025-07-31 12:18:59 +0300

    use React portal to display the modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,12 +1,15 @@
+import ReacDOM from "react-dom";
+
 import "./Modal.css";
 
 export default function Modal({ children, handleClose }) {
-  return (
+  return ReacDOM.createPortal(
     <div className="modal-backdrop">
       <div className="modal">
         {children}
         <button onClick={handleClose}>Close</button>
       </div>
-    </div>
+    </div>,
+    document.body
   );
 }
