9b42e803998c2ec95aa5cef305b2a89773beae42
Majd Aldein Alhourani <majd.aldein.alhourani@gmail.com>
2025-07-31 11:29:20 +0300

    display a list of events

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { useState } from "react";
+
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello React</div>;
+  const [events, setEvents] = useState([
+    { title: "Majd's birthday bash", id: 1 },
+    { title: "Bowser's live stream", id: 2 },
+    { title: "Race on Moo Moo Farm", id: 3 },
+  ]);
+
+  return (
+    <div className="App">
+      {events.map((event) => (
+        <div key={event.id}>
+          <h2>{event.title}</h2>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default App;
