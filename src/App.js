82261719cc42a552d47cb7a19ce0b645f3fafed8
Majd Aldein Alhourani <majd.aldein.alhourani@gmail.com>
2025-07-31 11:49:21 +0300

    toggle event list visibility with show/hide buttons

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [showEvents, setShowEvents] = useState(true);
+
   const [events, setEvents] = useState([
     { title: "Majd's birthday bash", id: 1 },
     { title: "Bowser's live stream", id: 2 },
@@ -19,12 +21,25 @@ function App() {
 
   return (
     <div className="App">
-      {events.map((event) => (
-        <div key={event.id}>
-          <h2>{event.title}</h2>
-          <button onClick={() => handleClick(event.id)}>Delete Event</button>
-        </div>
-      ))}
+      <div>
+        {showEvents && (
+          <button onClick={() => setShowEvents(false)}>Hide Events</button>
+        )}
+      </div>
+
+      <div>
+        {!showEvents && (
+          <button onClick={() => setShowEvents(true)}>Show Events</button>
+        )}
+      </div>
+
+      {showEvents &&
+        events.map((event) => (
+          <div key={event.id}>
+            <h2>{event.title}</h2>
+            <button onClick={() => handleClick(event.id)}>Delete Event</button>
+          </div>
+        ))}
     </div>
   );
 }
