6c5c3b3ba7810c4893d60fa8c4b11bb0dc3a3a5d
Majd Aldein Alhourani <majd.aldein.alhourani@gmail.com>
2025-07-31 11:33:28 +0300

    add delete functionality to remove events by ID

--- src/App.js
+++ src/App.js
@@ -9,11 +9,20 @@ function App() {
     { title: "Race on Moo Moo Farm", id: 3 },
   ]);
 
+  const handleClick = (id) => {
+    setEvents((preEvents) => {
+      return preEvents.filter((preEvent) => {
+        return preEvent.id !== id;
+      });
+    });
+  };
+
   return (
     <div className="App">
       {events.map((event) => (
         <div key={event.id}>
           <h2>{event.title}</h2>
+          <button onClick={() => handleClick(event.id)}>Delete Event</button>
         </div>
       ))}
     </div>
